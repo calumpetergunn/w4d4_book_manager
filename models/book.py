534c0964cf7ae1a56a6ba5fad32ef8a8e37e6c04
calumpetergunn <calumgunn@gmail.com>
2021-03-18 11:07:51 +0000

    inital commit

---
+++ models/book.py
@@ -0,0 +1,9 @@
+class Book:
+
+    def __init__(self, title, year, genre, author, id = None):
+        self.title = title
+        self.year = year
+        self.genre = genre
+        self.author = author
+        self.id = id
+        
