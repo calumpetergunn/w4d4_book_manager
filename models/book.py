fa10dd81aa725b71294aef0db8265af24ab528d7
calumpetergunn <calumgunn@gmail.com>
2021-03-18 14:45:39 +0000

    added purchased parameter

--- models/book.py
+++ models/book.py
@@ -1,9 +1,12 @@
 class Book:
 
-    def __init__(self, title, year, genre, author, id = None):
+    def __init__(self, title, year, genre, author, purchased = False, id = None):
         self.title = title
         self.year = year
         self.genre = genre
         self.author = author
+        self.purchased = purchased
         self.id = id
         
+    def mark_purchased(self):
+        self.purchased = True
