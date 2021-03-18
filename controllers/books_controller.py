1c0a6716fa79ee9fffa51c2abeaccf450c14ef2c
calumpetergunn <calumgunn@gmail.com>
2021-03-18 15:04:29 +0000

    trying to fix author issue

--- controllers/books_controller.py
+++ controllers/books_controller.py
@@ -1,8 +1,7 @@
 from flask import Flask, render_template, request, redirect
 from flask import Blueprint
+from repositories import book_repository, author_repository
 from models.book import Book
-import repositories.book_repository as book_repository
-import repositories.author_repository as author_repository
 
 books_blueprint = Blueprint("books", __name__)
 
